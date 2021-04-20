9472b5330e1e309a32a23c1d5ad11c88765d3dbc
Denniel Luis <sadiandenniel@gmail.com>
2021-04-20 21:26:12 +0800

    Modify `Meta` class

--- marshmallow-metaclass.py
+++ marshmallow-metaclass.py
@@ -5,46 +5,46 @@ class Meta(type):
     """The meta class."""
 
     def __new__(cls, name, bases, attrs):
-        """Custom __new__  method."""
+        """Custom __new__ method."""
+        new_class = super().__new__(cls, name, bases, attrs)
+
+        # get all marshmallow field attributes
+        FIELDS = dict()
+        for field_name in dir(new_class):
+            field = getattr(new_class, field_name)
+            if hasattr(field, '__module__') \
+                    and field.__module__ == 'marshmallow.fields':
+                FIELDS[field_name] = field
+
+        # create schema
+        SCHEMA = Schema.from_dict(FIELDS)()
 
         def __init__(self, **kwargs):
             """Custom __init__ method."""
-            # get all marshmallow fields
-            self.fields = []
-            for field in dir(self):
-                f = getattr(self, field)
-                if hasattr(f, '__module__') \
-                        and f.__module__ == 'marshmallow.fields':
-                    self.fields.append(field)
-
-            # create schema
-            self.field_keys = dict()
-            for field in self.fields:
-                self.field_keys[field] = getattr(self, field)
-            self.schema = Schema.from_dict(self.field_keys)()
-
             # desirialize
-            self.data = self.schema.load(kwargs)
+            self.data = self.SCHEMA.load(kwargs)
 
             # set attributes
             for key, val in self.data.items():
                 setattr(self, key, val)
-            for field in self.fields:
+            for field in self.FIELDS.keys():
                 if not field in self.data.keys():
                     setattr(self, field, None)
 
         def asdict(self):
             """Return dictionary."""
             dictionary = self.data
-            for field in self.fields:
+            for field in self.FIELDS.keys():
                 if not field in self.data.keys():
                     dictionary[field] = None
             return dictionary
 
-        # create new class and add custom __init__ method
-        new_class = super().__new__(cls, name, bases, attrs)
-        setattr(new_class, '__init__', __init__)
-        setattr(new_class, 'asdict', asdict)
+        # add attributes
+        new_class.__init__ = __init__
+        new_class.asdict = asdict
+        new_class.SCHEMA = SCHEMA
+        new_class.FIELDS = FIELDS
+
         return new_class
 
 

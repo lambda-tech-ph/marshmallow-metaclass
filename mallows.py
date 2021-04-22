a27caf10a5ae3fd8a678048a65573bd5aaaf3318
Denniel Luis <sadiandenniel@gmail.com>
2021-04-22 13:40:08 +0800

    Write `MetaMethods` class

--- mallows.py
+++ mallows.py
@@ -1,6 +1,31 @@
 from marshmallow import Schema, fields
 
 
+class MetaMethods:
+    """Separate class for the methods.
+    `Meta` will make new classes inherit from this.
+    """
+
+    def __init__(self, **kwargs):
+        """Custom __init__ method."""
+        # desirialize
+        self.data = self.SCHEMA.load(kwargs)
+        # set attributes
+        for key, val in self.data.items():
+            setattr(self, key, val)
+        for field in self.FIELDS.keys():
+            if not field in self.data.keys():
+                setattr(self, field, None)
+
+    def asdict(self):
+        """Return a dictionary version of the object."""
+        dictionary = self.data
+        for field in self.FIELDS.keys():
+            if not field in self.data.keys():
+                dictionary[field] = None
+        return dictionary
+
+
 class Meta(type):
     """The meta class.
     The new class will have these attributes:
